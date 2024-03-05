b0789b2231559c8981a4b64a5e044825616fe74a
Tilak Suthar <44789847+tiks98@users.noreply.github.com>
2024-03-04 20:30:54 -0500

    Linear Reg Model added
    Also updated some routes for better usage of API

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('users/create/', views.user_create, name='user-create'),
     path('users/<str:pk>/', views.user_detail, name='user-detail'),
     path('stocks/<str:identifier>/', views.stock_detail, name='stock_detail'),
+    path('stock-symbol/<str:name>/', views.get_symbol, name='stock_symbol'),
+    path('pre/<str:identifier>/', views.LinearRegModel, name='stock_predictions'),
 ]
