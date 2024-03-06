f36a6eece5df1b64a80b92df0b2209f6c4eb90a5
Tilak Suthar <44789847+tiks98@users.noreply.github.com>
2024-03-05 19:20:56 -0500

    Stock News and Info API endpoints
    Added two new endpoints

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('users/<str:pk>/', views.user_detail, name='user-detail'),
     path('stocks/<str:identifier>/', views.stock_detail, name='stock_detail'),
     path('stock-symbol/<str:name>/', views.get_symbol, name='stock_symbol'),
+    path('stock-news/<str:identifier>/', views.stock_news, name='stock_news'),
+    path('stock-info/<str:identifier>/', views.stock_info, name='stock_info'),
     path('pre/<str:identifier>/', views.LinearRegModel, name='stock_predictions'),
 ]
